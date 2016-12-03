50c0f12d4fb7410d078aea16043bb212ca5ab3e4
Louis MENIGAULT <lmenigau@e2r9p7.42.fr>
2016-12-03 14:06:39 +0100

    correct last fail

--- backtracking.c
+++ backtracking.c
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/22 14:53:02 by lmenigau          #+#    #+#             */
-/*   Updated: 2016/12/03 13:53:04 by lmenigau         ###   ########.fr       */
+/*   Updated: 2016/12/03 14:04:34 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,11 +121,10 @@ void	compute_grid_size(t_tetrimino *tetri, int max_count)
 
 	ft_memset(grid, '.', 16 * 16);
 	size = 2;
-	while (size * size <= max_count * 4)
+	while (size * size < max_count * 4)
 		size++;
 	data.tetri = tetri;
 	data.grid = grid;
-	data.size = size + 1;
 	while (backtracking(&data, 0, max_count, size) == 0)
 	{
 		size++;
